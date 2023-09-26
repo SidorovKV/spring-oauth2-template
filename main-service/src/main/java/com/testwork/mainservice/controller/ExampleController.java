13d288bc8d0c38cef16019f7321f3503decf1c5b
Sidorov K.V. <kvsidorov@gmail.com>
2023-09-26 16:04:03 +0300

    Add jwt claims reading on resource server example

--- main-service/src/main/java/com/testwork/mainservice/controller/ExampleController.java
+++ main-service/src/main/java/com/testwork/mainservice/controller/ExampleController.java
@@ -1,8 +1,13 @@
 package com.testwork.mainservice.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -20,5 +25,12 @@ public class ExampleController {
     @GetMapping("/authed")
     public String any() {
         return "Yeah! It's endpoint for anyone authed";
+    }
+
+    @GetMapping("/jwt/read")
+    public Map<String, Object> read(@AuthenticationPrincipal Jwt jwt) {
+        Map<String, Object> result = new HashMap<>(2);
+        result.put("principal", jwt.getClaims());
+        return result;
     }
 }
